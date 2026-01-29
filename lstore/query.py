85285143c0479f8e64a3ee6039d8c6d5cab35ccf
dannykcw <danny.kwong06@gmail.com>
2026-01-29 15:43:32 -0800

    Fix typos

--- lstore/query.py
+++ lstore/query.py
@@ -17,7 +17,7 @@ class Query:
     """
     # internal Method
     # Read a record with specified RID
-    # Returns True upon succesful deletion
+    # Returns True upon successful deletion
     # Return False if record doesn't exist or is locked due to 2PL
     """
     def delete(self, primary_key):
@@ -26,7 +26,7 @@ class Query:
     
     """
     # Insert a record with specified columns
-    # Return True upon succesful insertion
+    # Return True upon successful insertion
     # Returns False if insert fails for whatever reason
     """
     def insert(self, *columns):
@@ -52,7 +52,7 @@ class Query:
     # :param search_key: the value you want to search based on
     # :param search_key_index: the column index you want to search based on
     # :param projected_columns_index: what columns to return. array of 1 or 0 values.
-    # :param relative_version: the relative version of the record you need to retreive.
+    # :param relative_version: the relative version of the record you need to retrieve.
     # Returns a list of Record objects upon success
     # Returns False if record locked by TPL
     # Assume that select will never be called on a key that doesn't exist
@@ -63,7 +63,7 @@ class Query:
     
     """
     # Update a record with specified key and columns
-    # Returns True if update is succesful
+    # Returns True if update is successful
     # Returns False if no records exist with given key or if the target record cannot be accessed due to 2PL locking
     """
     def update(self, primary_key, *columns):
@@ -86,7 +86,7 @@ class Query:
     :param start_range: int         # Start of the key range to aggregate 
     :param end_range: int           # End of the key range to aggregate 
     :param aggregate_columns: int  # Index of desired column to aggregate
-    :param relative_version: the relative version of the record you need to retreive.
+    :param relative_version: the relative version of the record you need to retrieve.
     # this function is only called on the primary key.
     # Returns the summation of the given range upon success
     # Returns False if no record exists in the given range
@@ -96,7 +96,7 @@ class Query:
 
     
     """
-    incremenets one column of the record
+    increments one column of the record
     this implementation should work if your select and update queries already work
     :param key: the primary of key of the record to increment
     :param column: the column to increment
