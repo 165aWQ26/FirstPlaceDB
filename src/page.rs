6454819d1d4c155ecad78a91411c3bdda646c9ad
dannykcw <danny.kwong06@gmail.com>
2026-01-31 22:51:44 -0800

    implement page trait and nullable page

--- src/page.rs
+++ src/page.rs
@@ -6,56 +6,107 @@ pub enum PageError {
     IndexOutOfBounds(usize),
 }
 
-#[derive(Clone, Debug)]
-pub struct Page {
-    data: Vec<i64>,
-}
+pub trait GenericPage<T: Copy> {
+    fn data_mut(&mut self) -> &mut Vec<T>;
+    fn data(&self) -> &Vec<T>;
 
-impl Page {
-    pub fn new() -> Self {
-        Page {
-            data: Vec::with_capacity(MAX_RECORDS),
-        }
+    fn new() -> Self
+    where
+        Self: Sized,
+    {
+        Self::with_capacity(MAX_RECORDS)
     }
 
-    pub fn has_capacity(&self) -> bool {
-        self.data.len() < MAX_RECORDS
+    fn with_capacity(capacity: usize) -> Self
+    where
+        Self: Sized;
+
+    fn has_capacity(&self) -> bool {
+        self.data().len() < MAX_RECORDS
     }
 
-    pub fn write(&mut self, val: i64) -> Result<(), PageError> {
+    fn write(&mut self, val: T) -> Result<(), PageError> {
         if self.has_capacity() {
-            self.data.push(val);
+            self.data_mut().push(val);
             Ok(())
         } else {
             Err(PageError::Full)
         }
     }
 
-    // I don't think a page is allowed to be updated. Should be append only but I'll leave it here for now.
-    // TODO: Remove this?
-    pub fn update(&mut self, index: usize, val: i64) -> Result<(), PageError> {
-        if index >= self.data.len() {
-            Err(PageError::IndexOutOfBounds(index))
-        } else {
-            self.data[index] = val;
-            Ok(())
-        }
-    }
-
-    pub fn read(&self, index: usize) -> Result<i64, PageError> {
-        self.data
+    fn read(&self, index: usize) -> Result<T, PageError> {
+        self.data()
             .get(index)
             .copied()
             .ok_or(PageError::IndexOutOfBounds(index))
     }
 
-    pub fn len(&self) -> usize {
-        self.data.len()
+    fn len(&self) -> usize {
+        self.data().len()
     }
 }
 
+#[derive(Clone, Debug)]
+pub struct I64Page {
+    data: Vec<i64>,
+}
 
-impl Default for Page {
+impl GenericPage<i64> for I64Page {
+    fn new() -> Self {
+        I64Page {
+            data: Vec::with_capacity(MAX_RECORDS),
+        }
+    }
+
+    fn with_capacity(capacity: usize) -> Self {
+        I64Page {
+            data: Vec::with_capacity(capacity),
+        }
+    }
+
+    fn data_mut(&mut self) -> &mut Vec<i64> {
+        &mut self.data
+    }
+
+    fn data(&self) -> &Vec<i64> {
+        &self.data
+    }
+}
+
+impl Default for I64Page {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct NullableI64Page {
+    data: Vec<Option<i64>>,
+}
+
+impl GenericPage<Option<i64>> for NullableI64Page {
+    fn new() -> Self {
+        NullableI64Page {
+            data: Vec::with_capacity(MAX_RECORDS),
+        }
+    }
+
+    fn with_capacity(capacity: usize) -> Self {
+        NullableI64Page {
+            data: Vec::with_capacity(capacity),
+        }
+    }
+
+    fn data_mut(&mut self) -> &mut Vec<Option<i64>> {
+        &mut self.data
+    }
+
+    fn data(&self) -> &Vec<Option<i64>> {
+        &self.data
+    }
+}
+
+impl Default for NullableI64Page {
     fn default() -> Self {
         Self::new()
     }
@@ -65,22 +116,26 @@ impl Default for Page {
 mod tests {
     use super::*;
 
+    // I64Page Tests
+
     #[test]
     fn test_new_page_is_empty() {
-        let page = Page::new();
+        let page = I64Page::new();
         assert_eq!(page.len(), 0);
-        assert!(page.data.capacity() >= MAX_RECORDS);
+        assert!(page.data().capacity() >= MAX_RECORDS);
     }
 
     #[test]
     fn test_has_capacity_when_empty() {
-        let page = Page::new();
+        let page = I64Page::new();
         assert!(page.has_capacity());
     }
 
+    // Write
+
     #[test]
     fn test_write_single_value() {
-        let mut page = Page::new();
+        let mut page = I64Page::new();
         let result = page.write(42);
         assert!(result.is_ok());
         assert_eq!(page.len(), 1);
@@ -89,40 +144,33 @@ mod tests {
 
     #[test]
     fn test_write_until_full() {
-        let mut page = Page::new();
-
-        // Fill the page
+        let mut page = I64Page::new();
         for i in 0..MAX_RECORDS {
             assert!(page.has_capacity());
             assert!(page.write(i as i64).is_ok());
         }
-
         assert_eq!(page.len(), MAX_RECORDS);
         assert!(!page.has_capacity());
     }
 
     #[test]
     fn test_write_beyond_capacity_fails() {
-        let mut page = Page::new();
-
-        // Fill the page
+        let mut page = I64Page::new();
         for i in 0..MAX_RECORDS {
             page.write(i as i64).unwrap();
         }
-
-        // Try to write one more
         let result = page.write(999);
-        assert!(result.is_err());
         assert_eq!(result.unwrap_err(), PageError::Full);
     }
 
+    // Read
+
     #[test]
     fn test_read_valid_index() {
-        let mut page = Page::new();
+        let mut page = I64Page::new();
         page.write(10).unwrap();
         page.write(20).unwrap();
         page.write(30).unwrap();
-
         assert_eq!(page.read(0).unwrap(), 10);
         assert_eq!(page.read(1).unwrap(), 20);
         assert_eq!(page.read(2).unwrap(), 30);
@@ -130,41 +178,20 @@ mod tests {
 
     #[test]
     fn test_read_invalid_index() {
-        let mut page = Page::new();
+        let mut page = I64Page::new();
         page.write(42).unwrap();
-
-        assert!(page.read(1).is_err());
-        assert!(page.read(100).is_err());
+        assert_eq!(page.read(1).unwrap_err(), PageError::IndexOutOfBounds(1));
+        assert_eq!(page.read(100).unwrap_err(), PageError::IndexOutOfBounds(100));
     }
 
-    #[test]
-    fn test_update_valid_index() {
-        let mut page = Page::new();
-        page.write(10).unwrap();
-        page.write(20).unwrap();
-
-        assert!(page.update(0, 99).is_ok());
-        assert_eq!(page.read(0).unwrap(), 99);
-        assert_eq!(page.read(1).unwrap(), 20);
-    }
-
-    #[test]
-    fn test_update_invalid_index() {
-        let mut page = Page::new();
-        page.write(42).unwrap();
-
-        assert!(page.update(1, 99).is_err());
-        assert!(page.update(100, 99).is_err());
-    }
+    // Len
 
     #[test]
     fn test_len_updates_correctly() {
-        let mut page = Page::new();
+        let mut page = I64Page::new();
         assert_eq!(page.len(), 0);
-
         page.write(1).unwrap();
         assert_eq!(page.len(), 1);
-
         page.write(2).unwrap();
         assert_eq!(page.len(), 2);
     }
