fef49f6325c0ae823b42f86603e0f24711063b3e
dannykcw <danny.kwong06@gmail.com>
2026-02-08 03:51:19 -0800

    wip test
    Co-authored-by: rishiiyer11 <rishiiyer11@users.noreply.github.com>
    Co-authored-by: Alex <aalcar@users.noreply.github.com>
    Co-authored-by: AndrewDoesSomeCoding <AndrewDoesSomeCoding@users.noreply.github.com>
    Co-authored-by: ffrench <ryanffrench@users.noreply.github.com>

---
+++ tests/query_tests.rs
@@ -0,0 +1,50 @@
+#[cfg(test)]
+mod tests {
+    // use ::index::*;src
+    // use ::Table::*;src
+    // use ::Query::*;
+    use lstore::query::Query;
+    use lstore::table::Table;
+
+    
+
+    
+
+
+    #[test]
+    fn quick_test_all() {
+        let mut table: Table = Table::new(String::from("test"), 4, 5, 0);
+        let mut query: Query = Query::new(table);
+
+        let rec_one: Vec<Option<i64>> = vec![Some(1); 5];
+        let rec_two: Vec<Option<i64>> = vec![Some(2); 5];
+        let rec_three: Vec<Option<i64>> = vec![Some(3), Some(4), Some(5), Some(6), Some(7)];
+        let rec_four: Vec<Option<i64>> = vec![Some(4), Some(5), Some(6), Some(7), Some(8)];
+
+        query.insert(rec_one);
+        query.insert(rec_two);
+
+        let rid1 = query.table.indices[0].locate(1).unwrap();
+        
+        assert!(query.table.read(rid1[0]) == vec![Some(1); 5]);
+
+        let rid2 = query.table.indices[0].locate(2);
+        assert!(query.table.read(rid2.unwrap()[0]) == vec![Some(2); 5]);
+
+        query.insert(rec_three);
+
+        let ans:i64 = query.sum(1, 3, 3).unwrap();
+
+        assert!(ans == 8);
+
+        query.insert(rec_four);
+
+        query.delete(4);
+    
+        
+        let mut mask: [i64; 5] = [1, 0, 1, 0, 1];
+        let ans_list: Result<Vec<Vec<Option<i64>>> = query.select(1, 0, &mut mask)
+
+        for 
+    }
+}
