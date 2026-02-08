e5da966b8d7c545b50ce246bbc88418a8b813011
dannykcw <danny.kwong06@gmail.com>
2026-02-07 16:35:54 -0800

    Added db.rs

---
+++ src/db.rs
@@ -0,0 +1,24 @@
+use core::num;
+
+use rustc_hash::FxHashMap;
+
+use crate::table::{self, Table};
+
+struct Database {
+    tables: FxHashMap<String, Table>
+}
+
+impl Database {
+    pub fn create_table(&mut self, name: String, num_columns: usize, key_index: usize) {
+        let table = Table::new(name.clone(), num_columns, key_index);
+        self.tables.insert(name, table);
+    }
+
+    pub fn get_table(&self, name: &str) -> Option<&Table> {
+        self.tables.get(name)
+    }
+    pub fn drop_table(&mut self, name: &str) {
+        self.tables.remove(name);
+    }
+}
+
