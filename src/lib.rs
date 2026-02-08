195af9b62b80a17e3a79609b5fb9c800f249d453
aalcar <alcarazalex2006@gmail.com>
2026-02-07 17:07:04 -0800

    fix for merge

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use pyo3::prelude::*;
 
-pub mod page;
-pub mod index;
+mod index;
 
 /// A Python module implemented in Rust. The name of this module must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
