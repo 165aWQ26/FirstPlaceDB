2896389ed2d9f3a0e63fb4bcc3ca5d71fbe6a4b5
dannykcw <danny.kwong06@gmail.com>
2026-02-07 16:35:48 -0800

    Added key_index and num_column

--- src/table.rs
+++ src/table.rs
@@ -15,18 +15,24 @@ pub struct Table {
 
     pub rid : std::ops::RangeFrom<usize>,
 
+    pub key_index: usize,
+
+    pub num_columns: usize
+
 }
 
 impl Table {
     pub const PROJECTED_NUM_RECORDS : usize = 1200;
     pub const NUM_META_PAGES: usize = 3;
     //data_pages_per_collection is the total number of pages in a pagedir
-    pub fn new(&mut self, tableName: String, data_pages_per_collection: usize) -> Table {
+    pub fn new(table_name: String, data_pages_per_collection: usize, key_index: usize) -> Table {
         Self {
-            name: tableName,
+            name: table_name,
             pageRanges : PageRanges::new(data_pages_per_collection),
             pageDirectory : PageDirectory::default(),
             rid : 0..,
+            key_index: key_index,
+            num_columns: data_pages_per_collection
         }
     }
 }
