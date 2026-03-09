e270cabb1ed9ad14183d7416bf1251cb62e48eb8
dannykcw <danny.kwong06@gmail.com>
2026-03-09 11:35:28 -0700

    Add bindings to mod

--- src/bindings/mod.rs
+++ src/bindings/mod.rs
@@ -1,4 +1,9 @@
 mod core_db;
 mod core_query;
+mod core_table;
+mod core_index;
+
 pub use core_db::CoreDatabase;
 pub use core_query::CoreQuery;
+pub use core_table::CoreTable;
+pub use core_index::CoreIndex;
